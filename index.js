9b3020bfccfba0cfe2eafa296b297e6aa5235900
HafezFaruk <hmomarfaruk1454@gmail.com>
2021-10-29 18:36:49 +0600

    first commit fooddy24h

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0hj4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('geniusMechanic');
+        const sreviceCollection = database.collection('services');
+
+        // GET ALL DATA
+        app.get('/services', async (req, res) => {
+            const cursor = sreviceCollection.find({});
+            const services = await cursor.toArray(cursor);
+            res.send(services)
+
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res)=>{
+            const id = req.params.id;
+            console.log('getting single data',id);
+            const query = { _id: ObjectId(id) };
+            const service = await sreviceCollection.findOne(query);
+            console.log('find single services',service);
+            res.json(service);
+
+        });
+
+        // GET API UPDATE
+        app.get('/services/:id',async (req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const result = await sreviceCollection.insertOne(query);
+            console.log("load services",id);
+            res.send(result);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await sreviceCollection.insertOne(service);
+            res.json(result)
+        });
+
+        // DELETE API 
+        app.delete('/services/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const result = await sreviceCollection.deleteOne(query);
+            res.json(result);
+
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Server');
+});
+
+app.listen(port, () => {
+    console.log('Running Server on port', port);
+})
