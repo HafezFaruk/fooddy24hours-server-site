06185fe9e82ed7a1ccc53aa08faf5722298f478e
HafezFaruk <hmomarfaruk1454@gmail.com>
2021-12-29 04:32:32 +0600

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,104 +12,104 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0hj4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log('paysi', uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("paysi", uri);
 async function run() {
-    try {
-        await client.connect();
-        console.log('conneckt todad');
-        const database = client.db('fooddy24h');
-        const foodCollection = database.collection('services');
-        const ordersCollection = client
-            .db("fooddy24h")
-            .collection("orders");
+  try {
+    await client.connect();
+    const database = client.db("fooddy24h");
+    const foodCollection = database.collection("services");
+    const ordersCollection = database.collection("orders");
 
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log("hit the post api", service);
+      const result = await foodCollection.insertOne(service);
+      res.json(result);
+    });
 
-        //post api for services insert
-        app.post('/services', async (req, res) => {
-            const service = req.body;
-            console.log('hit the post api', service);
-            const result = await foodCollection.insertOne(service);
-            console.log(result);
-            res.json(result)
-        });
+    //GET API for show data
+    app.get("/services", async (req, res) => {
+      const cursor = foodCollection.find({}).limit(6);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
+    // GET Single Service id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific service", id);
+      const query = { _id: ObjectId(id) };
+      const service = await foodCollection.findOne(query);
+      res.json(service);
+    });
 
-        //GET API for show data
-        app.get('/services', async (req, res) => {
-            const cursor = foodCollection.find({}).limit(6);
-            const services = await cursor.toArray();
-            res.send(services);
-        });
+    // Add Orders API
+    app.post("/placeorder", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
 
-        // GET Single Service id
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log('getting specific service', id);
-            const query = { _id: ObjectId(id) };
-            const service = await foodCollection.findOne(query);
-            res.json(service);
-        })
+    app.get("/orders/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await ordersCollection.find(filter).toArray();
+      res.send(result);
+    });
 
-        // Add Orders API
-        app.post('/placeorder', async (req, res) => {
-            const order = req.body;
-            const result = await ordersCollection.insertOne(order);
-            res.json(result);
-        })
+    // dynamic api for update products
+    // app.get("/orders/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const order = await ordersCollection.findOne(query);
+    //   res.send(order);
+    // });
+      
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+      
+    // cancel an order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
 
-         // show my orders
-        app.get('/orders', async (req, res) => {
-            const cursor = ordersCollection.find({});
-            const product = await cursor.toArray();
-            res.send(product);
-        });
-
-         // cancel an order
-        app.delete('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await ordersCollection.deleteOne(query);
-            console.log('deleting user with id ', result);
-            res.json(result);
-          })
-
-            // dynamic api for update products
-            app.get('/orders/:id', async (req, res) => {
-                const id = req.params.id;
-                const query = { _id: ObjectId(id) };
-                const order = await ordersCollection.findOne(query);
-                console.log('load user with id: ', id);
-                res.send(order);
-            })
-
-
-        //update status
-        app.put('/update/:id', async (req, res) => {
-            const id = req.params.id;
-            const updatedOrder = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updateDoc = {
-            $set: {
-                status: updatedOrder.status,
-            },
-        };
-        const result = await ordersCollection.updateOne(filter, updateDoc, options)
-        console.log('updating', id)
-        res.json(result)
-        })
-
-           
-    } finally {
-        // await client.close();
-    }
+    //update status
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedOrder = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('running my khana food delivery  server')
-})
+app.get("/", (req, res) => {
+  res.send("Running My food delivery  server");
+});
 app.listen(port, () => {
-    console.log('listening on port', port);
+  console.log("listening on port", port);
 });
